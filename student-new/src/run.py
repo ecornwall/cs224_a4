3c42d858c31cd2997778c3606b65b75a0d2bc1c5
Elyse Cornwall <66961064+ecornwall@users.noreply.github.com>
2022-02-19 02:44:05 -0800

    2g

--- student-new/src/run.py
+++ student-new/src/run.py
@@ -58,7 +58,8 @@ if args.variant == 'vanilla':
     # TODO [part c]: Make some model here
     model = model.GPT(mconf)
 elif args.variant == 'synthesizer':
-    pass # TODO [part g]: Make some other model here
+    mconf.additive = True # use SynthesizerAttention instead
+    model = model.GPT(mconf)
 
 # From here on, your code should be identical independent of which
 # variant (vanilla or synthesizer) has been chosen.
